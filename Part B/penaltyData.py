cf3e96cb3debfce6e045690e59d01009d9405088
SlayerofevilYT <realslayerofevil@gmail.com>
2023-10-07 14:40:03 +1000

    Updated code a little. Added functions
    Will need to properly adapt how the functions work and the order in which they will be called. For now, the date search function will always be called first.

--- Part B/penaltyData.py
+++ Part B/penaltyData.py
@@ -13,50 +13,70 @@ start_date_input = input("Enter a start date (dd/mm/yyyy): ")
 end_date_input = input("Enter an end date (dd/mm/yyyy): ")
 keyword = input("Enter a keyword to search for or leave it blank: ")
 
-# Specify the name of the column you want to search in
-column_name = 'OFFENCE_MONTH'
+if keyword: # WILL NEED TO CHANGE FOR UI, Right now it just asks for the specific name of the column you want to search in
+    keywordSearchColumn = input("Enter the column you would like to search for a keyword in: ")
+
+# Specify the name of the column to search for dates
+dates_column_name = 'OFFENCE_MONTH'
 
 
-# Convert the start and end date strings to datetime objects if they were provided
-if start_date_input:
-    try:
-        start_date = datetime.strptime(start_date_input, "%d/%m/%Y")
-    except ValueError:
-        print("Invalid start date format. Please use the format dd/mm/yyyy.")
-        exit()
-else:
-    start_date = datetime.min  # Set to the minimum possible datetime if there was no date provided
 
-if end_date_input:
-    try:
-        end_date = datetime.strptime(end_date_input, "%d/%m/%Y")
-    except ValueError:
-        print("Invalid end date format. Please use the format dd/mm/yyyy.")
-        exit()
-else:
-    end_date = datetime.max  # Set to the maximum possible datetime if there was no date provided
+def DateSearch():
+    if start_date_input:
+        try:
+            start_date = datetime.strptime(start_date_input, "%d/%m/%Y")
+        except ValueError:
+            print("Invalid start date format. Please use the format dd/mm/yyyy.")
+            exit()
+        else:
+            start_date = datetime.min  # Set to the minimum possible datetime if there was no date provided
+
+        if end_date_input:
+            try:
+                end_date = datetime.strptime(end_date_input, "%d/%m/%Y")
+            except ValueError:
+                print("Invalid end date format. Please use the format dd/mm/yyyy.")
+                exit()
+        else:
+            end_date = datetime.max  # Set to the maximum possible datetime if there was no date provided
+
+        if start_date <= end_date:
+            result = df[(df[dates_column_name].apply(lambda date_str: start_date <= datetime.strptime(date_str, "%d/%m/%Y") <= end_date))]
+        else:
+            result = df  # If start date is greater than end date, select all rows
+
+        return result
+
+def KeywordSearch(result):
+    if result:
+        if keyword:
+            result = result[result[keywordSearchColumn].str.contains(keyword, case=False)] # If result and keyword are not null, search through result[]
+    elif result is None:
+        if keyword:
+            result = df[(df[keywordSearchColumn].str.contains(keyword, case=False))] # If result is null but keyword is not, search through df[]
 
 
-if start_date <= end_date:
-    result = df[
-        (df[column_name].apply(lambda date_str: start_date <= datetime.strptime(date_str, "%d/%m/%Y") <= end_date))
-    ]
-else:
-    result = df  # If start date is greater than end date, select all rows
+    return result
 
-if keyword:
-    result = result[result['OFFENCE_DESC'].str.contains(keyword, case = False)]
 
-# Display the search results
-if not result.empty:
-    print("Search results:")
-    print(result)
+def DisplayResults(result):
+    # Display the search results
+    if not result.empty:
+        print("Search results:")
+        print(result)
 
-    dates = pd.to_datetime(result[column_name], format="%d/%m/%Y")
-    values = result[column_name].to_numpy()
+        dates = pd.to_datetime(result[dates_column_name], format="%d/%m/%Y")
+        values = result[dates_column_name].to_numpy()
 
-    # Continue with matplotlib graphs
+        # Continue with matplotlib graphs
 
-else:
-    print("No matching records found.")
+    else:
+        print("No matching records found.")
 
+def StartSearch():
+    dateResult = DateSearch()
+    keywordResult = KeywordSearch(dateResult)
+    DisplayResults(keywordResult)
+
+
+StartSearch()
