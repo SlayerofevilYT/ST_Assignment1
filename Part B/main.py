ef3f1b224858fc6a0699fac198a25ab5829649c5
RadioactiveW <82916350+RadioactiveW@users.noreply.github.com>
2023-10-08 07:13:54 +1000

    added ui to program

--- Part B/main.py
+++ Part B/main.py
@@ -1,16 +1,155 @@
-# This is a sample Python script.
+# -*- coding: utf-8 -*-
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+###########################################################################
+## Python code generated with wxFormBuilder (version 3.10.1-0-g8feb16b3)
+## http://www.wxformbuilder.org/
+##
+## PLEASE DO *NOT* EDIT THIS FILE!
+###########################################################################
+
+import wx
+import wx.xrc
+import wx.adv
+import wx.grid
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+###########################################################################
+## Class Frame1
+###########################################################################
 
+class Frame1(wx.Frame):
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    def __init__(self, parent):
+        wx.Frame.__init__(self, parent, id=wx.ID_ANY, title=u"NSW Traffic Penalty Data", pos=wx.DefaultPosition,
+                          size=wx.Size(1492, 1000), style=wx.DEFAULT_FRAME_STYLE | wx.TAB_TRAVERSAL)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        self.SetSizeHints(wx.DefaultSize, wx.DefaultSize)
+
+        bSizer5 = wx.BoxSizer(wx.VERTICAL)
+
+        wSizer1 = wx.WrapSizer(wx.HORIZONTAL, wx.WRAPSIZER_DEFAULT_FLAGS)
+
+        self.m_togglereset = wx.ToggleButton(self, wx.ID_ANY, u"Reset Search", wx.DefaultPosition, wx.DefaultSize, 0)
+        wSizer1.Add(self.m_togglereset, 0, wx.ALL, 5)
+
+        self.m_button8 = wx.Button(self, wx.ID_ANY, u"Video Data", wx.DefaultPosition, wx.DefaultSize, 0)
+        wSizer1.Add(self.m_button8, 0, wx.ALIGN_CENTER_VERTICAL | wx.LEFT, 50)
+
+        self.m_text_search = wx.TextCtrl(self, wx.ID_ANY, u"Keyword\n\n", wx.DefaultPosition, wx.DefaultSize, 0)
+        wSizer1.Add(self.m_text_search, 0, wx.ALIGN_CENTER_VERTICAL | wx.LEFT, 50)
+
+        self.m_button_search_submit = wx.Button(self, wx.ID_ANY, u"Search", wx.DefaultPosition, wx.DefaultSize, 0)
+        wSizer1.Add(self.m_button_search_submit, 0, wx.ALIGN_CENTER_VERTICAL | wx.RIGHT, 50)
+
+        self.m_toggleBtn2 = wx.ToggleButton(self, wx.ID_ANY, u"Update Graphs", wx.DefaultPosition, wx.DefaultSize, 0)
+        wSizer1.Add(self.m_toggleBtn2, 0, wx.ALIGN_CENTER_VERTICAL | wx.RIGHT, 50)
+
+        self.m_static_startdate = wx.StaticText(self, wx.ID_ANY, u"Starting Date", wx.DefaultPosition, wx.DefaultSize,
+                                                0)
+        self.m_static_startdate.Wrap(-1)
+
+        wSizer1.Add(self.m_static_startdate, 0, wx.ALIGN_CENTER_VERTICAL, 5)
+
+        self.m_datePicker_start = wx.adv.DatePickerCtrl(self, wx.ID_ANY, wx.DefaultDateTime, wx.DefaultPosition,
+                                                        wx.DefaultSize, wx.adv.DP_DEFAULT)
+        wSizer1.Add(self.m_datePicker_start, 0, wx.ALL, 5)
+
+        self.m_static_enddata = wx.StaticText(self, wx.ID_ANY, u"Ending Date", wx.DefaultPosition, wx.DefaultSize, 0)
+        self.m_static_enddata.Wrap(-1)
+
+        wSizer1.Add(self.m_static_enddata, 0, wx.ALIGN_CENTER_VERTICAL, 5)
+
+        self.m_datePicker_end = wx.adv.DatePickerCtrl(self, wx.ID_ANY, wx.DefaultDateTime, wx.DefaultPosition,
+                                                      wx.DefaultSize, wx.adv.DP_DEFAULT)
+        wSizer1.Add(self.m_datePicker_end, 0, wx.ALL, 5)
+
+        self.m_button_date_submit = wx.Button(self, wx.ID_ANY, u"Submit", wx.DefaultPosition, wx.DefaultSize, 0)
+        wSizer1.Add(self.m_button_date_submit, 0, wx.ALIGN_CENTER_VERTICAL, 5)
+
+        bSizer5.Add(wSizer1, 0, wx.ALL | wx.EXPAND, 0)
+
+        bSizer7 = wx.BoxSizer(wx.VERTICAL)
+
+        self.m_grid_data = wx.grid.Grid(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, 0)
+
+        # Grid
+        self.m_grid_data.CreateGrid(20, 25)
+        self.m_grid_data.EnableEditing(False)
+        self.m_grid_data.EnableGridLines(True)
+        self.m_grid_data.EnableDragGridSize(False)
+        self.m_grid_data.SetMargins(0, 0)
+
+        # Columns
+        self.m_grid_data.AutoSizeColumns()
+        self.m_grid_data.EnableDragColMove(False)
+        self.m_grid_data.EnableDragColSize(True)
+        self.m_grid_data.SetColLabelAlignment(wx.ALIGN_CENTER, wx.ALIGN_CENTER)
+
+        # Rows
+        self.m_grid_data.EnableDragRowSize(True)
+        self.m_grid_data.SetRowLabelAlignment(wx.ALIGN_CENTER, wx.ALIGN_CENTER)
+
+        # Label Appearance
+
+        # Cell Defaults
+        self.m_grid_data.SetDefaultCellAlignment(wx.ALIGN_LEFT, wx.ALIGN_TOP)
+        bSizer7.Add(self.m_grid_data, 0, wx.ALL, 5)
+
+        bSizer5.Add(bSizer7, 1, wx.EXPAND, 5)
+
+        wSizer2 = wx.WrapSizer(wx.HORIZONTAL, wx.WRAPSIZER_DEFAULT_FLAGS)
+
+        self.m_panel1 = wx.Panel(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
+        wSizer2.Add(self.m_panel1, 1, wx.EXPAND | wx.ALL, 5)
+
+        self.m_panel2 = wx.Panel(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
+        wSizer2.Add(self.m_panel2, 1, wx.EXPAND | wx.ALL, 5)
+
+        self.m_panel3 = wx.Panel(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
+        wSizer2.Add(self.m_panel3, 1, wx.EXPAND | wx.ALL, 5)
+
+        bSizer5.Add(wSizer2, 1, wx.EXPAND, 5)
+
+        self.SetSizer(bSizer5)
+        self.Layout()
+
+        self.Centre(wx.BOTH)
+
+        # Connect Events
+        self.m_togglereset.Bind(wx.EVT_TOGGLEBUTTON, self.ToggleReset)
+        self.m_button8.Bind(wx.EVT_BUTTON, self.VideoData)
+        self.m_text_search.Bind(wx.EVT_TEXT, self.KeywordUpdate)
+        self.m_text_search.Bind(wx.EVT_TEXT_ENTER, self.KeywordUpdate)
+        self.m_button_search_submit.Bind(wx.EVT_BUTTON, self.KeywordSearch)
+        self.m_toggleBtn2.Bind(wx.EVT_TOGGLEBUTTON, self.GraphUpdate)
+        self.m_datePicker_start.Bind(wx.adv.EVT_DATE_CHANGED, self.StartDateUpdate)
+        self.m_datePicker_end.Bind(wx.adv.EVT_DATE_CHANGED, self.EndDateUpdate)
+        self.m_button_date_submit.Bind(wx.EVT_BUTTON, self.DateSearch)
+
+    def __del__(self):
+        pass
+
+    # Virtual event handlers, override them in your derived class
+    def ToggleReset(self, event):
+        event.Skip()
+
+    def VideoData(self, event):
+        event.Skip()
+
+    def KeywordUpdate(self, event):
+        event.Skip()
+
+    def KeywordSearch(self, event):
+        event.Skip()
+
+    def GraphUpdate(self, event):
+        event.Skip()
+
+    def StartDateUpdate(self, event):
+        event.Skip()
+
+    def EndDateUpdate(self, event):
+        event.Skip()
+
+    def DateSearch(self, event):
+        event.Skip()
--- Part B/penaltyData.py
+++ Part B/penaltyData.py
@@ -1,7 +1,17 @@
+import wx
+import wx.grid
 import pandas as pd
 import numpy as np
 from datetime import datetime
 import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.use('WXAgg')
+
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg
+from matplotlib.figure import Figure
+
+from main import Frame1 as Frame1
 
 # Load the CSV file into a pandas DataFrame
 df = pd.read_csv('penalty_data_set_2.csv')
@@ -13,7 +23,7 @@ start_date_input = input("Enter a start date (dd/mm/yyyy): ")
 end_date_input = input("Enter an end date (dd/mm/yyyy): ")
 keyword = input("Enter a keyword to search for or leave it blank: ")
 
-if keyword: # WILL NEED TO CHANGE FOR UI, Right now it just asks for the specific name of the column you want to search in
+if keyword:  # WILL NEED TO CHANGE FOR UI, Right now it just asks for the specific name of the column you want to search in
     keyword_search_column = input("Enter the column you would like to search for a keyword in: ")
 
 # Specify column names
@@ -21,72 +31,83 @@ dates_column_name = 'OFFENCE_MONTH'
 penalty_value_column_name = 'FACE_VALUE'
 
 
+class CalcFrame(MyFrame1):
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
+        self.Layout()
+        self.Show(True)
 
-def DateSearch():
-    if start_date_input:
-        try:
-            start_date = datetime.strptime(start_date_input, "%d/%m/%Y")
-        except ValueError:
-            print("Invalid start date format. Please use the format dd/mm/yyyy.")
-            exit()
-        else:
-            start_date = datetime.min  # Set to the minimum possible datetime if there was no date provided
-
-        if end_date_input:
+    def DateSearch():
+        if start_date_input:
             try:
-                end_date = datetime.strptime(end_date_input, "%d/%m/%Y")
+                start_date = datetime.strptime(start_date_input, "%d/%m/%Y")
             except ValueError:
-                print("Invalid end date format. Please use the format dd/mm/yyyy.")
+                print("Invalid start date format. Please use the format dd/mm/yyyy.")
                 exit()
-        else:
-            end_date = datetime.max  # Set to the maximum possible datetime if there was no date provided
+            else:
+                start_date = datetime.min  # Set to the minimum possible datetime if there was no date provided
 
-        if start_date <= end_date:
-            result = df[(df[dates_column_name].apply(lambda date_str: start_date <= datetime.strptime(date_str, "%d/%m/%Y") <= end_date))]
-        else:
-            result = df  # If start date is greater than end date, select all rows
+            if end_date_input:
+                try:
+                    end_date = datetime.strptime(end_date_input, "%d/%m/%Y")
+                except ValueError:
+                    print("Invalid end date format. Please use the format dd/mm/yyyy.")
+                    exit()
+            else:
+                end_date = datetime.max  # Set to the maximum possible datetime if there was no date provided
+
+            if start_date <= end_date:
+                result = df[(df[dates_column_name].apply(
+                    lambda date_str: start_date <= datetime.strptime(date_str, "%d/%m/%Y") <= end_date))]
+            else:
+                result = df  # If start date is greater than end date, select all rows
+
+            return result
+
+    def KeywordSearch(result):
+        if result.isna:
+            if keyword:
+                result = result[result[keyword_search_column].str.contains(keyword,
+                                                                           case=False)]  # If result and keyword are not null, search through result[]
+        elif result:
+            if keyword:
+                result = df[(df[keyword_search_column].str.contains(keyword,
+                                                                    case=False))]  # If result is null but keyword is not, search through df[]
 
         return result
 
-def KeywordSearch(result):
-    if result.isna:
-        if keyword:
-            result = result[result[keyword_search_column].str.contains(keyword, case=False)] # If result and keyword are not null, search through result[]
-    elif result:
-        if keyword:
-            result = df[(df[keyword_search_column].str.contains(keyword, case=False))] # If result is null but keyword is not, search through df[]
+    def DisplayResults(result):
+        # Display the search results
+        if not result.empty:
+            print("Search results:")
+            print(result)
+
+            dates = pd.to_datetime(result[dates_column_name], format="%d/%m/%Y")
+            values = result[dates_column_name].to_numpy()
+
+            # Continue with matplotlib graphs
+
+        else:
+            print("No matching records found.")
+
+    def AverageCost(result):
+        average_cost = result[penalty_value_column_name].mean()
+
+        return average_cost
+
+    def StartSearch():
+        dateResult = DateSearch()
+        keywordResult = KeywordSearch(dateResult)
+        average_cost = AverageCost(keywordResult)
+        DisplayResults(keywordResult)
+
+        # print("The average cost for these penalties are: " + average_cost)
+
+    StartSearch()
 
 
-    return result
-
-
-def DisplayResults(result):
-    # Display the search results
-    if not result.empty:
-        print("Search results:")
-        print(result)
-
-        dates = pd.to_datetime(result[dates_column_name], format="%d/%m/%Y")
-        values = result[dates_column_name].to_numpy()
-
-        # Continue with matplotlib graphs
-
-    else:
-        print("No matching records found.")
-
-def AverageCost(result):
-    average_cost = result[penalty_value_column_name].mean()
-
-    return average_cost
-
-def StartSearch():
-    dateResult = DateSearch()
-    keywordResult = KeywordSearch(dateResult)
-    average_cost = AverageCost(keywordResult)
-    DisplayResults(keywordResult)
-
-    # print("The average cost for these penalties are: " + average_cost)
-
-
-StartSearch()
+if __name__ == "__main__":
+    app = wx.App(False)
+    frame = CalcFrame()
+    app.MainLoop()
